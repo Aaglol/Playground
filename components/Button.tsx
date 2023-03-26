5416ec3742b78e4587f1a5b28cb43ce4c7d9296f
Aaglol <rlavoll@yahoo.no>
2023-03-26 17:51:39 +0200

    Update button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,12 +1,14 @@
-export const Button = ({title, onClick, type = 'success'}) => {
+import { FormEventHandler } from "react";
+
+export const Button = (props: {title: string, onClick: FormEventHandler<HTMLFormElement> | any, type: string}) => {
     
-    if (title === '') {
-        return 'Title must be set';
+    if (props.title === '') {
+        return null;
     }
     
     return (
-        <button className={`p-button button-${type}`} onClick={(e) => onClick(e)}>
-            {title}
+        <button className={`p-button button-${props.type ?? 'success'}`} onClick={(e) => props.onClick(e)}>
+            {props.title}
         </button>
-    )
+    );
 }
