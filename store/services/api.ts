1320dc8465f80f151c083d1833563c16d38f23cc
Aaglol <rlavoll@yahoo.no>
2023-03-28 03:16:16 +0200

    Support running from everywhere

--- store/services/api.ts
+++ store/services/api.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const CommunicationApi = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({ 
-        baseUrl: 'http://localhost:8081', 
+        baseUrl: typeof window !== 'undefined' ? 'http://' + window.location.hostname + ':8081/' : '', 
         credentials: "include"
     }),
     endpoints: () => ({}),
