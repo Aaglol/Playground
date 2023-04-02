2ddbc43c9593b48dca7de8c338c84ed500ee02bf
RobinLavoll <robin@mystore.no>
2023-04-02 21:37:35 +0200

    force rename

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import { useSelector } from 'react-redux';
 import { appIsLoggedIn } from '@/store/slices/appSlice';
 import Login from './Login';
-import { Frontpage } from './frontpage';
+import { Frontpage } from './Forside';
 
 export default function Home() {
     const isLoggedIn = useSelector(appIsLoggedIn);
