6253d30963ba5fc1131c9bf12a277f0f3ace095a
RobinLavoll <robin@mystore.no>
2023-04-02 21:34:47 +0200

    build works locally, hello Vercel?

--- pages/Frontpage.tsx
+++ pages/Frontpage.tsx
@@ -17,3 +17,5 @@ export function Frontpage() {
         </AppBody>
     );
 };
+
+export default Frontpage;
--- pages/Login.tsx
+++ pages/Login.tsx
@@ -30,6 +30,7 @@ export default function Login() {
         name: 'password',
         type: 'password',
         attribute: 'current-password',
+        className: '',
     }];
 
     const inputOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
