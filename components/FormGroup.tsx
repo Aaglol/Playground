0ef0edc199a135009ed0966bae9a0c59b66b1bc6
RobinLavoll <robin@mystore.no>
2023-04-02 21:14:53 +0200

    ts fixes

--- components/FormGroup.tsx
+++ components/FormGroup.tsx
@@ -1,9 +1,15 @@
-export const FormGroup = ({data, inputOnChange}) => {
-    const { title = '', name, type, attribute, className = '' } = data;
+export const FormGroup = (props: {data: {
+    title: string,
+    name: string,
+    type: string,
+    attribute: string,
+    className: string,
+}, inputOnChange: any}) => {
+    const { title = '', name, type, attribute, className = '' } = props.data;
     return (
         <div className="form-group">
             <label className="form-group-title">{title}</label>
-            <input type={type} className={`form-input ${className}`} name={name} autoComplete={attribute} onChange={inputOnChange} />
+            <input type={type} className={`form-input ${className}`} name={name} autoComplete={attribute} onChange={props.inputOnChange} />
         </div>
     );
 };
