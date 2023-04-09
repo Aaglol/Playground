fc67aa175b86c9d5615461618d9e7e100834bd89
Aaglol <rlavoll@yahoo.no>
2023-04-09 22:28:37 +0200

    Update api.ts

--- store/services/api.ts
+++ store/services/api.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const CommunicationApi = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({ 
-        baseUrl: typeof window !== 'undefined' ? 'https://79.160.181.14:8081/' : '', 
+        baseUrl: typeof window !== 'undefined' ? 'https://server.robin-dev.no:8081/' : '', 
         credentials: "include"
     }),
     endpoints: () => ({}),
