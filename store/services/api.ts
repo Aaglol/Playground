9a09a73da3d4dbc9a495fcf35e35a30793f4e089
Aaglol <rlavoll@yahoo.no>
2023-04-14 15:24:38 +0200

    woops it's not that secure yet

--- store/services/api.ts
+++ store/services/api.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const CommunicationApi = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({ 
-        baseUrl: typeof window !== 'undefined' ? 'https://server.robin-dev.no/' : '', 
+        baseUrl: typeof window !== 'undefined' ? 'http://server.robin-dev.no/' : '', 
         credentials: "include"
     }),
     endpoints: () => ({}),
