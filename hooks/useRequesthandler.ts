b797cd6e75af14846b9d0b6ba780fa7902ac0480
Aaglol <rlavoll@yahoo.no>
2023-04-14 15:22:21 +0200

    Update backend url

--- hooks/useRequesthandler.ts
+++ hooks/useRequesthandler.ts
@@ -1,16 +1,18 @@
 import axios from 'axios';
 
 export const useRequesthandler = async (URL: string, PATH: string, type: string, payload: object = {}) => {
+    const baseURL = 'http://server.robin-dev.no/'
+    
     const requestHandler = axios.create({
         withCredentials: true,
-        baseURL: URL
+        baseURL: baseURL
     });
 
     if (type === 'GET') {
-        return await requestHandler.get(URL + PATH);
+        return await requestHandler.get(baseURL + PATH);
     }
     if (type === 'POST') {
-        return await requestHandler.post(URL + PATH, payload);
+        return await requestHandler.post(baseURL + PATH, payload);
     }
     
     return false;
