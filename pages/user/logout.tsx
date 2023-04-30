6948918c114f911a0b4cc1c6d057ad5ad4d3179c
Aaglol <rlavoll@yahoo.no>
2023-05-01 01:43:58 +0200

    Update logout.tsx

--- pages/user/logout.tsx
+++ pages/user/logout.tsx
@@ -1,16 +1,19 @@
 import { AppBody } from "@/components/layout/AppBody"
 import { useEffect } from "react"
 import { useLogoutMutation } from "@/store/services/user";
+import { useSelector } from "react-redux";
+import { appCurrentUser } from "@/store/slices/appSlice";
 
 export const Logout = () => {
 
+    const user = useSelector(appCurrentUser);
     const [logout] = useLogoutMutation();
 
     useEffect(() => {
-        const handleLogout = async () => await logout().unwrap();
+        const handleLogout = async () => await logout({id: user.id}).unwrap();
         handleLogout();
         window.location.href = "/";
-    }, []);
+    }, [logout, user.id]);
 
     return ( 
         <AppBody>
