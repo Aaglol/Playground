2595f77519ae35faa817aa31b933bcf69b12c467
Aaglol <rlavoll@yahoo.no>
2023-05-06 18:04:34 +0200

    Update api.ts

--- store/services/api.ts
+++ store/services/api.ts
@@ -4,7 +4,13 @@ export const CommunicationApi = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({ 
         baseUrl: typeof window !== 'undefined' ? 'https://server.robin-dev.no/' : '', 
-        credentials: "include"
+        credentials: "include",
+        prepareHeaders: (headers) => {
+                headers.set("Access-Control-Allow-Origin", 'true');
+              
+    
+            return headers;
+        },
     }),
     endpoints: () => ({}),
 });
